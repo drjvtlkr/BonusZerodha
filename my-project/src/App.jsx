dcd616ed2c2bf3e721bcfea792c60bd70f216fe9
dheeraj <dheerajrvithalkar@gmail.com>
2024-04-17 23:55:58 +0530

    all pages added

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,17 +1,29 @@
 import React, { useEffect } from "react";
 import { Routes, Route , useNavigate} from "react-router-dom";
+import {NavBar} from "./components/navBar";
 import Login from "./pages/Login";
 import OTP from "./pages/OTP";
-import {NavBar} from "./components/navBar"
+import Dashboard from "./pages/navbarPages/Dashboard";
+import Orders from "./pages/navbarPages/Orders";
+import Holdings from "./pages/navbarPages/Holdings";
+import Positions from "./pages/navbarPages/Positions";
+import Bids from "./pages/navbarPages/Bids";
+import Funds from "./pages/navbarPages/Funds";
 
 const App = () => {
   
   return (
     <>
-    <NavBar/>
+    <NavBar />
       <Routes>
         <Route path="/" element={<Login/>}/>
         <Route path="/otp" element={<OTP/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/orders" element={<Orders/>}/>
+        <Route path="/holdings" element={<Holdings/>}/>
+        <Route path="/positions" element={<Positions/>}/>
+        <Route path="/bids" element={<Bids/>}/>
+        <Route path="/funds" element={<Funds/>}/>
       </Routes>
     </>
   );
--- my-project/src/components/navBar.tsx
+++ my-project/src/components/navBar.tsx
@@ -1,74 +1,70 @@
 import React from "react";
+import {Link } from "react-router-dom";
 import { BellIcon } from "@heroicons/react/24/outline";
 
 export const NavBar = () => {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
-          href="https://flowbite.com/"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
           <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8"
-            alt="Flowbite Logo"
+            src="zerodha.png"
+            className="h-20"
+            alt="Zerodha Logo"
           />
-        </a>
 
         <div
-          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          className="items-center justify-end hidden w-full md:flex md:w-auto md:order-1"
           id="navbar-user"
         >
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <a
-                href="#"
+              <Link
+                to="/dashboard"
                 className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-orange-700 md:p-0 md:dark:text-orange-500"
                 aria-current="page"
               >
                 Dashboard
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
+              <Link
+                to="orders"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Orders
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
+              <Link
+                to="holdings"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Holdings
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
+              <Link
+                to="positions"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Positions
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
+              <Link
+                to="bids"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Bids
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
+              <Link
+                to="funds"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Funds
-              </a>
+              </Link>
             </li>
             <button
               type="button"
