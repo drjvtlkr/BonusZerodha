b69efc64bed6b3863b26955219a0cc789ee01a34
dheeraj <dheerajrvithalkar@gmail.com>
2024-04-17 19:41:49 +0530

    login page styling done

--- my-project/src/pages/Login.jsx
+++ my-project/src/pages/Login.jsx
@@ -4,16 +4,16 @@ import { useNavigate } from "react-router-dom";
 const Login = () => {
   const naivgate = useNavigate();
   return (
-    <div>
-      <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8">
         <form className="space-y-6" action="#">
-          <h5 className="text-xl font-medium text-gray-900 dark:text-white">
-            Sign in to our platform
+          <h5 className="text-xl font-medium text-gray-900">
+            Sign in to Zerodha
           </h5>
           <div>
             <label
-              for="email"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900"
             >
               Your email
             </label>
@@ -21,15 +21,15 @@ const Login = () => {
               type="email"
               name="email"
               id="email"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-              placeholder="name@company.com"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5"
+              placeholder="your@email.com"
               required
             />
           </div>
           <div>
             <label
-              for="password"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-900"
             >
               Your password
             </label>
@@ -38,7 +38,7 @@ const Login = () => {
               name="password"
               id="password"
               placeholder="••••••••"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5"
               required
             />
           </div>
@@ -49,36 +49,36 @@ const Login = () => {
                   id="remember"
                   type="checkbox"
                   value=""
-                  className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                  className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-orange-300 "
                   required
                 />
               </div>
               <label
-                for="remember"
-                className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                htmlFor="remember"
+                className="ms-2 text-sm font-medium text-gray-900"
               >
                 Remember me
               </label>
             </div>
             <a
               href="#"
-              className="ms-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
+              className="ms-auto text-sm text-orange-600 hover:underline"
             >
-              Lost Password?
+              Forgot Password?
             </a>
           </div>
           <button
             type="submit"
-            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="w-full text-white bg-orange-600 hover:bg-orange-400 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
             onClick={()=> naivgate(`/otp`)}
           >
             Login to your account
           </button>
-          <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+          <div className="text-sm font-medium text-gray-500 ">
             Not registered?{" "}
             <a
               href="#"
-              className="text-blue-700 hover:underline dark:text-blue-500"
+              className="text-orange-600 hover:underline "
             >
               Create account
             </a>
