16d117523e59d21193a5bbf2e3bc50c50afb8e6e
Anjum bahamani <anjumbahamani@gmail.com>
2024-04-18 23:59:12 +0530

    done

--- my-project/src/components/navBar.tsx
+++ my-project/src/components/navBar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { BellIcon } from "@heroicons/react/24/outline";
+import zerodhaLogo from "../assets/zerodhaLogo.png";
 
 export const NavBar = () => {
   return (
@@ -9,7 +10,7 @@ export const NavBar = () => {
         <div>
           
         </div>
-        <img src="kite-logo.jpg" className="h-10" alt="Zerodha Logo" />
+        <img src={zerodhaLogo} className="h-7" alt="Zerodha Logo" />
 
         <div
           className="items-center justify-end hidden w-full md:flex md:w-auto md:order-1"
@@ -51,7 +52,7 @@ export const NavBar = () => {
             </li>
             <li>
               <Link
-                to="./bids"
+                to="/home/bids"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Bids
@@ -59,7 +60,7 @@ export const NavBar = () => {
             </li>
             <li>
               <Link
-                to="funds"
+                to="/home/funds"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Funds
--- my-project/src/pages/navbarPages/Bids.jsx
+++ my-project/src/pages/navbarPages/Bids.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
-
+import alert from "../../assets/alert.jpg"
+import console from "../../assets/console.jpg"
 const Bids = () => {
   return (
-    <div>Bids</div>
+    <>
+      <div>
+        {/* <div className='bg-yellow-100 border-2 border-yellow-300 p-3 sm:p-4 md:p-5 lg:p-6 xl:p-8'>
+          <p className='text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl'>
+            The equity, F&O, and currency markets will remain closed today, on account of Ram Navami. Commodity markets will be closed for the first half and trading will be open only in the evening session (5:00 pm onwards).
+            <span className='text-blue-500'>Read more</span>
+          </p>
+        </div> */}
+
+        <div className='flex flex-row'>
+          <p className='ml-10 hover:border-b-2 hover:border-orange-500 hover:text-orange-500'> Auctions</p>
+          <p className='ml-10  hover:border-b-2 hover:border-orange-500 hover:text-orange-500'>IPO</p>
+          <p className='ml-10  hover:border-b-2 hover:border-orange-500 hover:text-orange-500'>Govt.securities</p>
+        </div>
+        <div className='text-gray-300 border'></div>
+        <div className="flex flex-col items-center justify-center mt-10">
+          <div className="mb-4">
+            <img src={alert} alt="" width={100} height={120} />
+          </div>
+          <div className="text-center">
+            <p className='text-gray-500 text-sm w-25'>
+              There are no stocks for auctions yet.
+              The auction market opens at 2 PM. Stocks
+            </p>
+            <p className='text-gray-500 text-sm w-25'>
+              eligible to be sold in the auction will be listed here. <span className='text-blue-600'>Read more.</span></p>
+            <button type="button" class="text-white bg-blue-600 hover:bg-blue-400 focus:ring-4 mt-5 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Get Started</button>
+            <div className='flex flex-row item-center justify-center mt-5 '>
+              <div className="">
+                <img src={console} alt="" width={20} height={20} />
+              </div>
+              <p className='text-blue-600 ml-2 hover:text-orange-700'>Analytics</p>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </>
+
   )
 }
 
