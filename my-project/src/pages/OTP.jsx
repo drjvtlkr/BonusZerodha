11ab28c939babf63708d821dec1201ad3c2dbfb6
Jagadish03 <jagadishmunavalli35@gmail.com>
2024-04-18 17:44:21 +0530

    otp

--- my-project/src/pages/OTP.jsx
+++ my-project/src/pages/OTP.jsx
@@ -2,68 +2,65 @@ import React from "react";
 
 const OTP = () => {
   return (
-    <>
-      <div>
-        <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-12">
-          <div className="relative w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 mb-4">
-            <div className="mx-auto flex w-full max-w-md flex-col space-y-16">
-              <div className="flex flex-col items-center justify-center text-center space-y-2">
-                <div className="font-semibold text-3xl">
-                  <img
-                    className="w-24 h-24 bg-red-200 rounded-full text-xl font-thin"
-                    alt="user image"
+    <div className="flex items-center justify-center min-h-screen bg-gray-50 py-10">
+      <div className="w-full max-w-xs p-3 bg-white border border-gray-200 rounded-lg shadow sm:p-4">
+        <div className="mx-auto flex flex-col space-y-6">
+          <div className="flex flex-col items-center justify-center text-center space-y-2">
+            <div className="font-semibold text-xl">
+              <img
+                className="w-20 h-20 bg-red-200 rounded-full text-lg font-thin"
+                alt="user image"
+              />
+              <p className="text-2xl font-semibold py-3">CE4528</p>
+            </div>
+            
+          </div>
+
+          <div className="flex flex-col items-center space-y-6">
+            <div className="flex flex-row items-center justify-center w-full">
+              <div className="max-w-xs">
+                <div className="h-14">
+                  <input
+                    className="otp-input w-full h-full flex flex-col items-center justify-center text-center outline-none rounded-lg border border-gray-200 text-sm bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
+                    type="text"
+                    maxLength={6}
+                    placeholder="******"
                   />
-                  <p className="text-2xl font-light">User Name</p>
                 </div>
-                <div className="flex flex-row text-sm font-medium text-gray-400">
-                  <p>We have sent a code to your email user@gmail.com</p>
+              </div>
+            </div>
+
+            <p className="text-sm text-gray-400 font-sans text-justify px-3">
+            Open the Kite mobile app on your phone to generate the 2FA App Code. <span className="text-blue-600">Need help?</span> 
+            </p>
+
+            <div className="flex flex-col space-y-3 w-full">
+              <div className="flex justify-center">
+                {" "}
+                {/* Add this div for centering */}
+                <div className="py-4">
+                  <button className="w-full outline-none py-3 px-16 bg-orange-500 border-none text-white text-lg shadow-sm">
+                    continue
+                  </button>
                 </div>
               </div>
 
-              
-
-              <div>
-                <div className="flex flex-col items-center space-y-16">
-                  <div class="flex flex-row items-center justify-between mx-auto w-full max-w-xs">
-                    <div class=" h-16">
-                      <input
-                        class="otp-input w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-lg border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
-                        type="text"
-                        maxLength={6}
-                        placeholder="******"
-                      />
-                    </div>
-                  </div>
-                  <p className="text-gray-400">
-                    Enter your 6 digit otp sent on your email
-                  </p>
-
-                  <div className="flex flex-col space-y-5">
-                    <div>
-                      <button className="flex flex-row items-center justify-center text-center w-full border rounded-xl outline-none py-5 bg-orange-600 border-none text-white text-sm shadow-sm">
-                        Verify Account
-                      </button>
-                    </div>
-
-                    <div className="flex flex-row items-center justify-center text-center text-sm font-medium space-x-1 text-gray-500">
-                      <p>Didn't recieve code?</p>{" "}
-                      <a
-                        className="flex flex-row items-center text-orange-600"
-                        href="http://"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Resend
-                      </a>
-                    </div>
-                  </div>
-                </div>
+              <div className="flex flex-row pb-5 items-center justify-center text-center text-xs font-medium space-x-1 text-gray-500">
+                <p>Problem with Mobile App Code?</p>{" "}
+                <a
+                  className="flex flex-row items-center text-orange-600"
+                  href="http://"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Resend
+                </a>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
