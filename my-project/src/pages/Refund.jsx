85214724e3cffcbdc75126fdffed96289f3a573c
Jagadish03 <jagadishmunavalli35@gmail.com>
2024-04-19 09:36:28 +0530

    Final changes

--- my-project/src/pages/Refund.jsx
+++ my-project/src/pages/Refund.jsx
@@ -3,6 +3,7 @@ import { MdBarChart } from "react-icons/md";
 import { FaChevronRight } from "react-icons/fa";
 import { CiClock1 } from "react-icons/ci";
 import { DateRangePicker, Stack } from "rsuite";
+import { Table } from "flowbite-react";
 // import './styles.css';
 const Refund = () => {
   const [selectedDateRange, setSelectedDateRange] = useState(null);
@@ -18,39 +19,144 @@ const Refund = () => {
 
     function drawChart() {
       var dataTable = new window.google.visualization.DataTable();
-      dataTable.addColumn({ type: 'date', id: 'Date' });
-      dataTable.addColumn({ type: 'number', id: 'Won/Loss' });
+      dataTable.addColumn({ type: "date", id: "Date" });
+      dataTable.addColumn({ type: "number", id: "Won/Loss" });
       dataTable.addRows([
-        [ new Date(2024, 3, 13), 37032 ],
-        [ new Date(2024, 3, 14), 38024 ],
-        [ new Date(2024, 3, 15), 38024 ],
-        [ new Date(2024, 3, 16), 38108 ],
-        [ new Date(2024, 3, 17), 38229 ],
+        [new Date(2023, 0, 1), 10000],
+        [new Date(2023, 0, 2), 20000],
+        [new Date(2023, 0, 3), 30000],
+        [new Date(2023, 0, 4), 40000],
+        [new Date(2023, 0, 5), 50000],
+        [new Date(2023, 0, 6), 60000],
+        [new Date(2023, 0, 7), 70000],
+        [new Date(2023, 0, 8), 80000],
+        [new Date(2023, 0, 9), 90000],
 
-        [ new Date(2024, 0, 13), 27032 ],
-        [ new Date(2024, 1, 14), 58024 ],
-        [ new Date(2024, 1, 15), 8024 ],
-        [ new Date(2024, 1, 16), 58108 ],
-        [ new Date(2024, 1, 17), 58229 ],
+        [new Date(2023, 1, 10), 100000],
+        [new Date(2023, 1, 11), 110000],
+        [new Date(2023, 1, 12), 120000],
+        [new Date(2023, 1, 13), 130000],
+        [new Date(2023, 1, 14), 140000],
+        [new Date(2023, 1, 15), 150000],
+        [new Date(2023, 1, 16), 160000],
+        [new Date(2023, 1, 17), 170000],
+        [new Date(2023, 1, 19), 190000],
+        [new Date(2023, 1, 20), 200000],
 
-        
-        // Many rows omitted for brevity.
-        [ new Date(2023, 9, 4), 38177 ],
-        [ new Date(2023, 9, 5), 38705 ],
-        [ new Date(2023, 9, 12), 38210 ],
-        [ new Date(2023, 9, 13), 38029 ],
-        [ new Date(2023, 9, 19), 38823 ],
-        [ new Date(2023, 9, 23), 38345 ],
-        [ new Date(2023, 9, 24), 38436 ],
-        [ new Date(2023, 9, 30), 38447 ]
+        [new Date(2023, 2, 10), 10000],
+        [new Date(2023, 2, 11), 110000],
+        [new Date(2023, 2, 13), 130000],
+        [new Date(2023, 2, 14), 140000],
+        [new Date(2023, 2, 16), 150000],
+        [new Date(2023, 2, 18), 170000],
+        [new Date(2023, 2, 19), 190000],
+        [new Date(2023, 2, 25), 20000],
+
+        [new Date(2023, 4, 10), 10000],
+        [new Date(2023, 4, 1), 110000],
+        [new Date(2023, 4, 3), 130000],
+        [new Date(2023, 4, 12), 140000],
+        [new Date(2023, 4, 20), 170000],
+        [new Date(2023, 4, 16), 150000],
+        [new Date(2023, 4, 18), 170000],
+        [new Date(2023, 4, 19), 190000],
+        [new Date(2023, 4, 25), 20000],
+
+        [new Date(2023, 11, 2), 20000],
+        [new Date(2023, 11, 3), 30000],
+        [new Date(2023, 11, 4), 40000],
+        [new Date(2023, 11, 8), 80000],
+        [new Date(2023, 11, 9), 90000],
+        [new Date(2023, 11, 10), 100000],
+        [new Date(2023, 11, 11), 110000],
+        [new Date(2023, 11, 12), 120000],
+        [new Date(2023, 11, 14), 140000],
+        [new Date(2023, 11, 15), 150000],
+        [new Date(2023, 11, 16), 160000],
+        [new Date(2023, 11, 17), 170000],
+        [new Date(2023, 11, 18), 180000],
+        [new Date(2023, 11, 19), 190000],
+        [new Date(2023, 11, 20), 200000],
+
+        // ------------------------------2024
+        [new Date(2024, 3, 13), 37032],
+        [new Date(2024, 3, 14), 38024],
+        [new Date(2024, 3, 15), 38024],
+        [new Date(2024, 3, 16), 38108],
+        [new Date(2024, 3, 17), 38229],
+
+        [new Date(2024, 7, 10), 10000],
+        [new Date(2024, 7, 1), 110000],
+        [new Date(2024, 7, 3), 130000],
+        [new Date(2024, 7, 12), 140000],
+        [new Date(2024, 7, 20), 170000],
+        [new Date(2024, 7, 16), 150000],
+        [new Date(2024, 7, 18), 170000],
+        [new Date(2024, 7, 19), 190000],
+        [new Date(2024, 7, 25), 20000],
+
+        [new Date(2024, 0, 13), 27032],
+        [new Date(2024, 1, 14), 58024],
+        [new Date(2024, 1, 15), 8024],
+        [new Date(2024, 1, 16), 58108],
+        [new Date(2024, 1, 17), 58229],
+
+        [new Date(2024, 10, 10), 10000],
+        [new Date(2024, 10, 1), 110000],
+        [new Date(2024, 10, 3), 130000],
+        [new Date(2024, 10, 12), 140000],
+        [new Date(2024, 10, 20), 170000],
+        [new Date(2024, 10, 16), 150000],
+        [new Date(2024, 10, 18), 170000],
+        [new Date(2024, 10, 19), 190000],
+        [new Date(2024, 10, 25), 20000],
+
+        [new Date(2024, 5, 1), 10000],
+        [new Date(2024, 5, 2), 20000],
+        [new Date(2024, 5, 3), 30000],
+        [new Date(2024, 5, 4), 40000],
+        [new Date(2024, 5, 5), 50000],
+        [new Date(2024, 5, 6), 60000],
+        [new Date(2024, 5, 7), 70000],
+        [new Date(2024, 5, 8), 80000],
+        [new Date(2024, 5, 9), 90000],
+        [new Date(2024, 5, 10), 100000],
+        [new Date(2024, 5, 11), 110000],
+        [new Date(2024, 5, 12), 120000],
+        [new Date(2024, 5, 13), 130000],
+        [new Date(2024, 5, 14), 140000],
+        [new Date(2024, 5, 15), 150000],
+        [new Date(2024, 5, 16), 160000],
+        [new Date(2024, 5, 17), 170000],
+        [new Date(2024, 5, 18), 180000],
+        [new Date(2024, 5, 19), 190000],
+        [new Date(2024, 5, 20), 200000],
+
+        [new Date(2023, 9, 4), 38177],
+        [new Date(2023, 9, 5), 38705],
+        [new Date(2023, 9, 12), 38210],
+        [new Date(2023, 9, 13), 38029],
+        [new Date(2023, 9, 19), 38823],
+        [new Date(2023, 9, 23), 38345],
+        [new Date(2023, 9, 24), 38436],
+        [new Date(2023, 9, 30), 38447],
       ]);
 
-      var chart = new window.google.visualization.Calendar(document.getElementById('calendar_basic'));
+      var chart = new window.google.visualization.Calendar(
+        document.getElementById("calendar_basic")
+      );
 
       var options = {
-        // title: "Red Sox Attendance",
-        height: 350,
-        colorAxis: { colors: ['red', 'orange', 'yellow', 'green'] }
+        height: 450,
+        calendar: {
+          cellSize: 20, // Adjust cell size for better visibility
+          monthLabel: {
+            fontSize: 14, // Adjust font size of month labels
+            color: "#000", // Set color for month labels
+            bold: true, // Make month labels bold
+          },
+        },
+        colorAxis: { colors: ["red", "orange", "yellow", "green"] },
       };
 
       chart.draw(dataTable, options);
@@ -127,17 +233,188 @@ const Refund = () => {
       </div>
 
       <div className="py-5">
-      {selectedDateRange && (
-        <div className="mt-4 flex justify-end px-5">
-            <CiClock1 className="mr-3 text-xl"/>
-          <div>
-            <p> {selectedDateRange[0].toISOString().substring(0, 10)} to {selectedDateRange[1].toISOString().substring(0, 10)}</p>
+        {selectedDateRange && (
+          <div className="mt-4 flex justify-end px-5">
+            <CiClock1 className="mr-3 text-xl" />
+            <div>
+              <p>
+                {" "}
+                {selectedDateRange[0].toISOString().substring(0, 10)} to{" "}
+                {selectedDateRange[1].toISOString().substring(0, 10)}
+              </p>
+            </div>
           </div>
-        </div>
-      )}
+        )}
       </div>
 
-      <div id="calendar_basic" style={{ width: '100%', height: '350px' }}></div>
+      <div
+        className="mb-6"
+        id="calendar_basic"
+        style={{ width: "100%", marginLeft: "20px" }}
+      ></div>
+
+      <div class="grid grid-cols-5 gap-4 border bg-gray-300 py-8 mb-6 text-lg">
+        <div class="flex flex-col items-center justify-center">
+          <p>Realised P&L</p>
+          <p className="font-bold text-red-500 text-4xl">-28.94L</p>
+        </div>
+        <div class="flex flex-col items-center justify-center">
+          <p>Charges & taxes</p>
+          <p className="font-bold text-4xl">8.22L</p>
+        </div>
+        <div class="flex flex-col items-center justify-center">
+          <p>Other credits & debits</p>
+          <p className="font-bold text-4xl">141.6</p>
+        </div>
+        <div class="flex flex-col items-center justify-center">
+          <p>Net realised P&L</p>
+          <p className="font-bold text-red-500 text-4xl">-37.16L</p>
+        </div>
+        <div class="flex flex-col items-center justify-center">
+          <p>Unrealised P&L</p>
+          <p className="font-bold text-4xl">0</p>
+        </div>
+      </div>
+
+      <div className="overflow-x-auto mt-4">
+      <table className="min-w-full divide-y divide-gray-200">
+        <thead className="bg-gray-50">
+          <tr>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Symbol
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              QTY
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Buy avg
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Buy Value
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              sell Avg
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Sell value
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Realised P&L
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              unrealised P&L
+            </th>
+          </tr>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          <tr>
+            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+              AXISBIBANK23JUN980CE
+            </td>
+            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+              2400
+            </td>
+            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+              13.15
+            </td>
+            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+              31,560.00
+            </td>
+            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                12.60
+            </td>
+            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                30,240.00
+            </td>
+            <td className="px-6 py-4 whitespace-nowrap text-sm text-red-500">
+                -1,3200.00
+            </td>
+            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                -
+            </td>
+          </tr>
+
+          <tr>
+  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+    AXISBIBANK23JUN980CE
+  </td>
+  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+    1500
+  </td>
+  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+    10.75
+  </td>
+  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+    16,125.00
+  </td>
+  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+    10.00
+  </td>
+  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+    15,000.00
+  </td>
+  <td className="px-6 py-4 whitespace-nowrap text-sm text-red-500">
+    -1,125.00
+  </td>
+  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+    -
+  </td>
+</tr>
+
+<tr>
+  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+  BANKNIFTY23JUN980CE
+  </td>
+  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+    2000
+  </td>
+  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+    15.25
+  </td>
+  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+    30,500.00
+  </td>
+  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+    14.50
+  </td>
+  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+    29,000.00
+  </td>
+  <td className="px-6 py-4 whitespace-nowrap text-sm text-red-500">
+    -1,500.00
+  </td>
+  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+    -
+  </td>
+</tr>
+          
+        </tbody>
+      </table>
+    </div>
 
     </div>
   );
