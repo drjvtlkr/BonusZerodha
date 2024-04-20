0075dd0561d6eecf3698ce915d084298b69eea91
Jagadish03 <jagadishmunavalli35@gmail.com>
2024-04-20 23:01:58 +0530

    refund

--- my-project/src/pages/Refund.jsx
+++ my-project/src/pages/Refund.jsx
@@ -8,186 +8,193 @@ import "./Refund.css";
 
 const Refund = () => {
   const [selectedDateRange, setSelectedDateRange] = useState(null);
-
+  const [showCalendar, setShowCalendar] = useState(false);
+  const [chartLoaded, setChartLoaded] = useState(false);
   const handleDateRangeChange = (value) => {
     setSelectedDateRange(value);
   };
 
   useEffect(() => {
-    // Load Google Charts library
-    google.charts.load("current", { packages: ["calendar"] });
-    google.charts.setOnLoadCallback(drawChart);
-
-    function drawChart() {
-      var dataTable = new window.google.visualization.DataTable();
-      dataTable.addColumn({ type: "date", id: "Date" });
-      dataTable.addColumn({ type: "number", id: "Won/Loss" });
-      dataTable.addRows([
-        [new Date(2023, 0, 1), 10000],
-        [new Date(2023, 0, 2), 20000],
-        [new Date(2023, 0, 3), 30000],
-        [new Date(2023, 0, 4), 40000],
-        [new Date(2023, 0, 5), 50000],
-        [new Date(2023, 0, 6), 60000],
-        [new Date(2023, 0, 7), 70000],
-        [new Date(2023, 0, 8), 80000],
-        [new Date(2023, 0, 9), 90000],
-
-        [new Date(2023, 1, 10), 100000],
-        [new Date(2023, 1, 11), 110000],
-        [new Date(2023, 1, 12), 120000],
-        [new Date(2023, 1, 13), 130000],
-        [new Date(2023, 1, 14), 140000],
-        [new Date(2023, 1, 15), 150000],
-        [new Date(2023, 1, 16), 160000],
-        [new Date(2023, 1, 17), 170000],
-        [new Date(2023, 1, 19), 190000],
-        [new Date(2023, 1, 20), 200000],
-
-        [new Date(2023, 2, 10), 10000],
-        [new Date(2023, 2, 11), 110000],
-        [new Date(2023, 2, 13), 130000],
-        [new Date(2023, 2, 14), 140000],
-        [new Date(2023, 2, 16), 150000],
-        [new Date(2023, 2, 18), 170000],
-        [new Date(2023, 2, 19), 190000],
-        [new Date(2023, 2, 25), 20000],
-
-        [new Date(2023, 4, 10), 10000],
-        [new Date(2023, 4, 1), 110000],
-        [new Date(2023, 4, 3), 130000],
-        [new Date(2023, 4, 12), 140000],
-        [new Date(2023, 4, 20), 170000],
-        [new Date(2023, 4, 16), 150000],
-        [new Date(2023, 4, 18), 170000],
-        [new Date(2023, 4, 19), 190000],
-        [new Date(2023, 4, 25), 20000],
-
-        [new Date(2023, 11, 2), 20000],
-        [new Date(2023, 11, 3), 30000],
-        [new Date(2023, 11, 4), 40000],
-        [new Date(2023, 11, 8), 80000],
-        [new Date(2023, 11, 9), 90000],
-        [new Date(2023, 11, 10), 100000],
-        [new Date(2023, 11, 11), 110000],
-        [new Date(2023, 11, 12), 120000],
-        [new Date(2023, 11, 14), 140000],
-        [new Date(2023, 11, 15), 150000],
-        [new Date(2023, 11, 16), 160000],
-        [new Date(2023, 11, 17), 170000],
-        [new Date(2023, 11, 18), 180000],
-        [new Date(2023, 11, 19), 190000],
-        [new Date(2023, 11, 20), 200000],
-
-        // ------------------------------2024
-        [new Date(2023, 3, 13), 37032],
-        [new Date(2023, 3, 14), 38024],
-        [new Date(2023, 3, 15), 38024],
-        [new Date(2023, 3, 16), 38108],
-        [new Date(2023, 3, 17), 38229],
-        [new Date(2023, 7, 10), 10000],
-        [new Date(2023, 7, 1), 110000],
-        [new Date(2023, 7, 3), 130000],
-        [new Date(2023, 7, 12), 140000],
-        [new Date(2023, 7, 20), 170000],
-        [new Date(2023, 7, 16), 150000],
-        [new Date(2023, 7, 18), 170000],
-        [new Date(2023, 7, 19), 190000],
-        [new Date(2023, 7, 25), 20000],
-
-        [new Date(2023, 6, 13), 27032],
-        [new Date(2023, 6, 14), 58024],
-        [new Date(2023, 6, 15), 8024],
-        [new Date(2023, 6, 16), 58108],
-        [new Date(2023, 6, 17), 58229],
-        [new Date(2023, 8, 13), 27032],
-        [new Date(2023, 8, 14), 58024],
-        [new Date(2023, 8, 15), 8024],
-        [new Date(2023, 8, 16), 58108],
-        [new Date(2023, 8, 17), 58229],
-
-        [new Date(2023, 10, 10), 10000],
-        [new Date(2023, 10, 1), 110000],
-        [new Date(2023, 10, 3), 130000],
-        [new Date(2023, 10, 12), 140000],
-        [new Date(2023, 10, 20), 170000],
-        [new Date(2023, 10, 16), 150000],
-        [new Date(2023, 10, 18), 170000],
-        [new Date(2023, 10, 19), 190000],
-        [new Date(2023, 10, 25), 20000],
-
-        [new Date(2023, 5, 1), 10000],
-        [new Date(2023, 5, 2), 20000],
-        [new Date(2023, 5, 3), 30000],
-        [new Date(2023, 5, 4), 40000],
-        [new Date(2023, 5, 5), 50000],
-        [new Date(2023, 5, 6), 60000],
-        [new Date(2023, 5, 7), 70000],
-        [new Date(2023, 5, 8), 80000],
-        [new Date(2023, 5, 9), 90000],
-        [new Date(2023, 5, 10), 100000],
-        [new Date(2023, 5, 11), 110000],
-        [new Date(2023, 5, 12), 120000],
-        [new Date(2023, 5, 13), 130000],
-        [new Date(2023, 5, 14), 140000],
-        [new Date(2023, 5, 15), 150000],
-        [new Date(2023, 5, 16), 160000],
-        [new Date(2023, 5, 17), 170000],
-        [new Date(2023, 5, 18), 180000],
-        [new Date(2023, 5, 19), 190000],
-        [new Date(2023, 5, 20), 200000],
-
-        [new Date(2023, 9, 4), 38177],
-        [new Date(2023, 9, 5), 38705],
-        [new Date(2023, 9, 12), 38210],
-        [new Date(2023, 9, 13), 38029],
-        [new Date(2023, 9, 19), 38823],
-        [new Date(2023, 9, 23), 38345],
-        [new Date(2023, 9, 24), 38436],
-        [new Date(2023, 9, 30), 38447],
-      ]);
-
-      var chart = new window.google.visualization.Calendar(
-        document.getElementById("calendar_basic")
-      );
-
-      var options = {
-        height: 300,
-        calendar: {
-          cellSize: 20, // Adjust cell size for better visibility
-          monthLabel: {
-            fontSize: 14, // Adjust font size of month labels
-            bold: true, // Make month labels bold
-          },
-        },
-        colorAxis: {
-          colors: ["#FF6666", "red", "green", "#33FF33"],
-        },
-        tooltip: { isHtml: true }, // Enable HTML tooltips
-      };
-
-      // Set tooltip content
-      google.visualization.events.addListener(
-        chart,
-        "onmouseover",
-        function (e) {
-          var date = new Date(e.date);
-          var tooltip = (
-            <div style="padding:10px">
-              Gross Realised P&L on ' + date.toDateString() + ': ' + e.value + '
-            </div>
-          );
-          chart.setSelection([{ row: e.row }]);
-          chart.draw(
-            dataTable,
-            Object.assign({}, options, { tooltip: tooltip })
-          );
-        }
-      );
-
-      chart.draw(dataTable, options);
+    if (showCalendar && !chartLoaded) {
+      // Load Google Charts library only if not already loaded
+      google.charts.load("current", { packages: ["calendar"] });
+      google.charts.setOnLoadCallback(() => {
+        setChartLoaded(true); // Set chartLoaded to true when library is loaded
+      });
     }
-  }, []);
+  }, [showCalendar, chartLoaded]);
+
+  useEffect(() => {
+    if (chartLoaded && showCalendar) {
+      // Draw the chart only when the library is loaded and showCalendar is true
+      drawChart();
+    }
+  }, [chartLoaded, showCalendar]);
+
+  function drawChart() {
+    var dataTable = new window.google.visualization.DataTable();
+    dataTable.addColumn({ type: "date", id: "Date" });
+    dataTable.addColumn({ type: "number", id: "Won/Loss" });
+    dataTable.addRows([
+      [new Date(2023, 0, 1), 10000],
+      [new Date(2023, 0, 2), 20000],
+      [new Date(2023, 0, 3), 30000],
+      [new Date(2023, 0, 4), 40000],
+      [new Date(2023, 0, 5), 50000],
+      [new Date(2023, 0, 6), 60000],
+      [new Date(2023, 0, 7), 70000],
+      [new Date(2023, 0, 8), 80000],
+      [new Date(2023, 0, 9), 90000],
+
+      [new Date(2023, 1, 10), 100000],
+      [new Date(2023, 1, 11), 110000],
+      [new Date(2023, 1, 12), 120000],
+      [new Date(2023, 1, 13), 130000],
+      [new Date(2023, 1, 14), 140000],
+      [new Date(2023, 1, 15), 150000],
+      [new Date(2023, 1, 16), 160000],
+      [new Date(2023, 1, 17), 170000],
+      [new Date(2023, 1, 19), 190000],
+      [new Date(2023, 1, 20), 200000],
+
+      [new Date(2023, 2, 10), 10000],
+      [new Date(2023, 2, 11), 110000],
+      [new Date(2023, 2, 13), 130000],
+      [new Date(2023, 2, 14), 140000],
+      [new Date(2023, 2, 16), 150000],
+      [new Date(2023, 2, 18), 170000],
+      [new Date(2023, 2, 19), 190000],
+      [new Date(2023, 2, 25), 20000],
+
+      [new Date(2023, 4, 10), 10000],
+      [new Date(2023, 4, 1), 110000],
+      [new Date(2023, 4, 3), 130000],
+      [new Date(2023, 4, 12), 140000],
+      [new Date(2023, 4, 20), 170000],
+      [new Date(2023, 4, 16), 150000],
+      [new Date(2023, 4, 18), 170000],
+      [new Date(2023, 4, 19), 190000],
+      [new Date(2023, 4, 25), 20000],
+
+      [new Date(2023, 11, 2), 20000],
+      [new Date(2023, 11, 3), 30000],
+      [new Date(2023, 11, 4), 40000],
+      [new Date(2023, 11, 8), 80000],
+      [new Date(2023, 11, 9), 90000],
+      [new Date(2023, 11, 10), 100000],
+      [new Date(2023, 11, 11), 110000],
+      [new Date(2023, 11, 12), 120000],
+      [new Date(2023, 11, 14), 140000],
+      [new Date(2023, 11, 15), 150000],
+      [new Date(2023, 11, 16), 160000],
+      [new Date(2023, 11, 17), 170000],
+      [new Date(2023, 11, 18), 180000],
+      [new Date(2023, 11, 19), 190000],
+      [new Date(2023, 11, 20), 200000],
+
+      // ------------------------------2024
+      [new Date(2023, 3, 13), 37032],
+      [new Date(2023, 3, 14), 38024],
+      [new Date(2023, 3, 15), 38024],
+      [new Date(2023, 3, 16), 38108],
+      [new Date(2023, 3, 17), 38229],
+      [new Date(2023, 7, 10), 10000],
+      [new Date(2023, 7, 1), 110000],
+      [new Date(2023, 7, 3), 130000],
+      [new Date(2023, 7, 12), 140000],
+      [new Date(2023, 7, 20), 170000],
+      [new Date(2023, 7, 16), 150000],
+      [new Date(2023, 7, 18), 170000],
+      [new Date(2023, 7, 19), 190000],
+      [new Date(2023, 7, 25), 20000],
+
+      [new Date(2023, 6, 13), 27032],
+      [new Date(2023, 6, 14), 58024],
+      [new Date(2023, 6, 15), 8024],
+      [new Date(2023, 6, 16), 58108],
+      [new Date(2023, 6, 17), 58229],
+      [new Date(2023, 8, 13), 27032],
+      [new Date(2023, 8, 14), 58024],
+      [new Date(2023, 8, 15), 8024],
+      [new Date(2023, 8, 16), 58108],
+      [new Date(2023, 8, 17), 58229],
+
+      [new Date(2023, 10, 10), 10000],
+      [new Date(2023, 10, 1), 110000],
+      [new Date(2023, 10, 3), 130000],
+      [new Date(2023, 10, 12), 140000],
+      [new Date(2023, 10, 20), 170000],
+      [new Date(2023, 10, 16), 150000],
+      [new Date(2023, 10, 18), 170000],
+      [new Date(2023, 10, 19), 190000],
+      [new Date(2023, 10, 25), 20000],
+
+      [new Date(2023, 5, 1), 10000],
+      [new Date(2023, 5, 2), 20000],
+      [new Date(2023, 5, 3), 30000],
+      [new Date(2023, 5, 4), 40000],
+      [new Date(2023, 5, 5), 50000],
+      [new Date(2023, 5, 6), 60000],
+      [new Date(2023, 5, 7), 70000],
+      [new Date(2023, 5, 8), 80000],
+      [new Date(2023, 5, 9), 90000],
+      [new Date(2023, 5, 10), 100000],
+      [new Date(2023, 5, 11), 110000],
+      [new Date(2023, 5, 12), 120000],
+      [new Date(2023, 5, 13), 130000],
+      [new Date(2023, 5, 14), 140000],
+      [new Date(2023, 5, 15), 150000],
+      [new Date(2023, 5, 16), 160000],
+      [new Date(2023, 5, 17), 170000],
+      [new Date(2023, 5, 18), 180000],
+      [new Date(2023, 5, 19), 190000],
+      [new Date(2023, 5, 20), 200000],
+
+      [new Date(2023, 9, 4), 38177],
+      [new Date(2023, 9, 5), 38705],
+      [new Date(2023, 9, 12), 38210],
+      [new Date(2023, 9, 13), 38029],
+      [new Date(2023, 9, 19), 38823],
+      [new Date(2023, 9, 23), 38345],
+      [new Date(2023, 9, 24), 38436],
+      [new Date(2023, 9, 30), 38447],
+    ]);
+
+    var chart = new window.google.visualization.Calendar(
+      document.getElementById("calendar_basic")
+    );
+
+    var options = {
+      height: 300,
+      // width: 1000,
+      calendar: {
+        cellSize: 26, // Adjust cell size for better visibility
+        monthLabel: {
+          fontSize: 14, // Adjust font size of month labels
+          bold: true, // Make month labels bold
+        },
+      },
+      colorAxis: {
+        colors: ["#FF6666", "red", "green", "#33FF33"],
+      },
+      tooltip: { isHtml: true }, // Enable HTML tooltips
+    };
+
+    // Set tooltip content
+    google.visualization.events.addListener(chart, "onmouseover", function (e) {
+      var date = new Date(e.date);
+      var tooltip = (
+        <div style="padding:10px">
+          Gross Realised P&L on ' + date.toDateString() + ': ' + e.value + '
+        </div>
+      );
+      chart.setSelection([{ row: e.row }]);
+      chart.draw(dataTable, Object.assign({}, options, { tooltip: tooltip }));
+    });
+
+    chart.draw(dataTable, options);
+  }
+
   return (
     <div className="px-5">
       <div className="flex text-2xl text-gray-500">
@@ -253,7 +260,10 @@ const Refund = () => {
         </div>
 
         {/* Button */}
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold mt-8 mb-2 px-4 rounded">
+        <button
+          onClick={() => setShowCalendar(!showCalendar)}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold mt-8 mb-2 px-4 rounded"
+        >
           <FaChevronRight />
         </button>
       </div>
@@ -272,6 +282,14 @@ const Refund = () => {
           </div>
         )}
       </div>
+
+      {showCalendar && (
+        <div
+          className=""
+          id="calendar_basic"
+          style={{ width: "100%", marginLeft: "20px" }}
+        ></div>
+      )}
 
       <div
         className=""
