ec06147d26640b899e04753233bf8f10b61e8514
Jagadish03 <jagadishmunavalli35@gmail.com>
2024-04-21 16:51:32 +0530

    solving error

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/BonusZerodha/my-project/dist/index.html",
+  // base: "/BonusZerodha/my-project/dist/index.html",
   plugins: [react()],
   assetsInclude: ['**/*.csv', '**/*.xlsx'],
 })
